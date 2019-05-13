1f36771ae27e737daaf6972144a58a412862381f
Khavya Seshadri <ks4049@rit.edu>
2019-05-12 22:32:15 -0400

    Distance Calculator

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const hostName ="localhost";
+const port = 5000;
+const express = require('express');
+const app = express();
+var path = require('path');
+app.use(express.static(path.join(__dirname,'assets')));
+//Home page
+app.get('/', function(req, res){
+	console.log("Hello Khavya Seshadri!!!");
+	res.sendFile(path.join(__dirname,'/views/home.html'));
+});
+
+app.listen(port, hostName);
+console.log("Web Service API application");
