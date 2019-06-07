f92b93d765e911445cd17ffb194324efcb7ed691
Khavya Seshadri <ks4049@rit.edu>
2019-06-06 20:44:13 -0400

    Changed port

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require('http');
-const hostName ="localhost";
-const port = 5000;
+const port = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 var path = require('path');
@@ -11,5 +10,5 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname,'/views/home.html'));
 });
 
-app.listen(port);
+app.listen(port, () => console.log(`Listening on ${port}`));
 console.log("Web Service API application");
